949c8320f55a7670fd628e378f6c8411cd254ca9
Hans Aguilar <hans.christian.aguilar@gmail.com>
2022-05-19 09:48:26 -0500

    Tweaks

--- astralforge/assets/scripts/form.js
+++ astralforge/assets/scripts/form.js
@@ -1,74 +1,7 @@
-/*"use strict"
+'use strict'
 
-const $form = document.getElementById('form')
-const $firstName = document.getElementById('first-name')
-const $lastName = document.getElementById('last-name')
-const $age = document.getElementById('age')
-const $email = document.getElementById('email')
-const $submit = document.getElementById('submit')
 
-const emailPattern = /^(?=^.{8,}$)[-_A-Za-z0-9]+([_.-][a-zA-Z0-9]+)*@[A-Za-z0-9]+([.-][a-zA-Z0-9]+)*\.[A-Za-z]{2,}$/
-
-$submit.addEventListener('click', () => {
-    let firstName = $firstName.value.trim(); 
-    let age = $age.value.trim();
-    let lastName = $lastName.value.trim();
-    let email = $email.value.trim();
-
-    let message = '';
-    let valid = true;
-    let count = 0;
-
-    if (firstName === '') {
-        message += "first name is required\n"
-        valid = false;
-        count++;
-    }
-
-    if (firstName === '') {
-        message += "last name is required\n"
-        valid = false;
-        count++;
-    }
-
-    if (firstName === '') {
-        message += "age is required\n"
-        count++;
-    } else {
-        let ageInt = parseInt(age);
-
-        if (isNaN(age) || (ageInt < 16 || ageInt > 120)) {
-            message += "age is not valid\n"
-            valid = false;
-        }
-    }
-
-    if (email === '') {
-        message += "last name is required\n"
-        count++;
-    } else {
-        if(!emailPattern.test(email)) {
-            valid = false;
-        }
-    }
-
-    if (count === 4) {
-        alert('all fields are required')
-    } else if (!valid) {
-        alert(message);
-    } else {
-        $form.submit()
-        console.log('form submitted')
-    }
-
-    alert(message);
-    alert(count);
-});
-*/
-//new 
-//new
-//new
-
+//Form
 const $form = document.forms['registration'];
 const $firstName = document.querySelector('#first-name');
 const $lastName = document.querySelector('#last-name');
@@ -130,9 +63,9 @@ $send.addEventListener('click', function() {
 	} else if (!valid && issueCount <= 4) {
 		error.innerText = issues;
 	} else {
-        firstName = '';
-        lastName = '';
-		email = '';
+        $firstName.value = '';
+        $lastName.value = '';
+		$email.value = '';
 		error.innerText = 'Success!';
 	}
 });
